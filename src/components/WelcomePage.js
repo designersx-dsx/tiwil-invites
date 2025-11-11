1090b18528bfcae808f5c3ee40500024efb99012
Ronak <ronakkaushik.dx@gmail.com>
2025-11-11 11:36:33 +0530

    eleven lab agent removed

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import './Welcome.css'; // You'll create this for styling
-import ElevenLabsWidget from './widget';
-import { useEffect,useRef } from 'react';
+import React from "react";
+import "./Welcome.css"; // You'll create this for styling
+import ElevenLabsWidget from "./widget";
+import { useEffect, useRef } from "react";
 function WelcomePage() {
+  return (
+    <>
+      {" "}
+      {/* <ElevenLabsWidget /> */}
+      <div className="welcome-container">
+        <img src="/images/TiwilLOGO1.png" alt="Tiwil Logo" className="logo" />
+        <h1>Welcome to Tiwil</h1>
+        <p>Plan your celebrations and share special moments with ease.</p>
 
-
-  return (<>   <ElevenLabsWidget/>
-    <div className="welcome-container">
-      <img src="/images/TiwilLOGO1.png" alt="Tiwil Logo" className="logo" />
-      <h1>Welcome to Tiwil</h1>
-      <p>Plan your celebrations and share special moments with ease.</p>
-      
-      <a
-        href="https://play.google.com/store/apps/details?id=com.tiwil.app" // replace with your actual link
-        target="_blank"
-        rel="noopener noreferrer"
-        className="download-button"
-      >
-        <img src="/images/playstore-svgrepo-com.svg" alt="Play Store" />
-        Download on Play Store
-      </a>
-    </div>
+        <a
+          href="https://play.google.com/store/apps/details?id=com.tiwil.app" // replace with your actual link
+          target="_blank"
+          rel="noopener noreferrer"
+          className="download-button"
+        >
+          <img src="/images/playstore-svgrepo-com.svg" alt="Play Store" />
+          Download on Play Store
+        </a>
+      </div>
     </>
   );
 }
